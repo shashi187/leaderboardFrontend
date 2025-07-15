f4108e946ff4c57e3d765d992eaccd2870c41bd6
shashiraj <darkshashi187@gmail.com>
2025-07-15 11:42:07 +0530

    fix

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://leaderboardbackend-vhp0.onrender.com'
+  baseURL: 'https://leaderboardbackend-vhp0.onrender.com/api'
 });
 
 export default api;
