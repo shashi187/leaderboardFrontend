28b8ca90527e659a1615991365ab822ca8ce00d6
shashiraj <darkshashi187@gmail.com>
2025-07-16 11:50:21 +0530

    readme

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://leaderboardbackend-vhp0.onrender.com/api'
+  baseURL: 'https://leaderboardbackend-production-a5b8.up.railway.app/api'
 });
 
 export default api;
